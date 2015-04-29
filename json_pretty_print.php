6ea2203cf0f3398d48d19cecbadbab07dc1dbc50
Claus Tøndering <claus@ezer.dk>
2015-04-29 09:59:42 +0200

    json_pretty_print.php now accepts data from stdin

--- json_pretty_print.php
+++ json_pretty_print.php
@@ -14,15 +14,16 @@ ini_set('track_errors', true);
 mb_internal_encoding('UTF-8');
 
 
-if ($_SERVER['argc']!==3 || ($_SERVER['argv'][1]!=='-p' && $_SERVER['argv'][1]!=='-u')) {
-    print "Usage: php (-p|-u) json_pretty_print.php <input file>\n";
+if (($_SERVER['argc']!==2 && $_SERVER['argc']!==3) || ($_SERVER['argv'][1]!=='-p' && $_SERVER['argv'][1]!=='-u')) {
+    print "Usage: php (-p|-u) json_pretty_print.php [<input file>]\n";
     print "       -p for pretty\n";
     print "       -u for ugly\n";
     die;
 }
 
+$filename = $_SERVER['argc']===3 ? $_SERVER['argv'][2] : 'php://stdin';
 
-$input = @file_get_contents($_SERVER['argv'][2]) or die ("Failed opening file {$_SERVER['argv'][2]}:\nError was '$php_errormsg'\n");
+$input = @file_get_contents($filename) or die ("Failed opening file {$filename}:\nError was '$php_errormsg'\n");
 
 $props = json_decode($input);
 
