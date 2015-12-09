d7643e697adff94749191389fc0032c513a45a95
Claus Tøndering <claus@ezer.dk>
2015-12-09 08:50:31 +0100

    Fixed error when importing from SHEBANQ

--- myapp/language/da/shebanq_lang.php
+++ myapp/language/da/shebanq_lang.php
@@ -5,3 +5,4 @@ $lang['missing_shebanq_version'] = 'Manglende SHEBANQ-version';
 $lang['version_does_not_exist'] = 'Version %s findes ikke';
 $lang['shebanq_error'] = 'Fejl:';
 $lang['no_query_with_id'] = 'Ingen forespørgsel med ID %s';
+$lang['shebanq_malformed_quotes'] = 'Ikke-matchende anførselstegn i MQL';
--- myapp/language/english/shebanq_lang.php
+++ myapp/language/english/shebanq_lang.php
@@ -5,3 +5,4 @@ $lang['missing_shebanq_version'] = 'Missing SHEBANQ version';
 $lang['version_does_not_exist'] = 'Version %s does not exist';
 $lang['shebanq_error'] = 'Error:';
 $lang['no_query_with_id'] = 'No query with ID %s';
+$lang['shebanq_malformed_quotes'] = 'Unmatched quotation marks in MQL';
