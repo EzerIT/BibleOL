873d4756f84816adcf2e1b1c174dcfa3deb48c32
Claus Tøndering <claus@ezer.dk>
2016-03-03 11:28:40 +0100

    "Folder" changed to "mappe"

--- myapp/language/da/file_manager_lang.php
+++ myapp/language/da/file_manager_lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['file_mgmt'] = 'Filhåndtering';  // Text in title bar
-$lang['create_folder'] = 'Opret folder';  // Text in title bar
-$lang['delete_folder'] = 'Slet folder';  // Text in title bar
+$lang['create_folder'] = 'Opret mappe';  // Text in title bar
+$lang['delete_folder'] = 'Slet mappe';  // Text in title bar
 $lang['copy_or_delete_files'] = 'Kopiér eller slet filer'; // Text in title bar
 $lang['insert_files'] = 'Indsæt Filer'; // Text in title bar
 $lang['upload_files'] = 'Upload filer'; // Text in title bar
@@ -12,13 +12,13 @@ $lang['download_exercise'] = 'Download øvelse'; // Text in title bar
 $lang['rename_exercise'] = 'Omdøb øvelse'; // Text in title bar
 $lang['exercise_file_mgmt'] = 'Administration af øvelsesfiler';
 $lang['file_mgmt_description'] = 
-    '<p>Her kan du uploade eller slette øvelsesfiler, og du kan oprette og slette foldere til
+    '<p>Her kan du uploade eller slette øvelsesfiler, og du kan oprette og slette mapper til
      filerne.</p>
-     <p>NB: Du kan kun slette en folder hvis den er tom.</p>
-     <p>Under knappen »Redigér synlighed« kan du styre hvem der kan se indholdet af hver folder.</p>
+     <p>NB: Du kan kun slette en mappe hvis den er tom.</p>
+     <p>Under knappen »Redigér synlighed« kan du styre hvem der kan se indholdet af hver mappe.</p>
      <p>Øvelsesfiler kan desuden skabes med det Windows-baserede PC-program PLOTLearner.
      Øvelsesfilerne har filtypen ».3et«.</p>';
-$lang['illegal_char_folder'] = 'Ulovligt tegn i folders navn';
+$lang['illegal_char_folder'] = 'Ulovligt tegn i mappes navn';
 $lang['missing_src_info'] = 'Manglende oplysninger om original';
 $lang['missing_dest_info'] = 'Manglende oplysninger om destination';
 $lang['file_exists'] = 'Destinationsfilen \'%s\' eksisterer allerede. Slet eller omdøb den.';
@@ -31,28 +31,28 @@ $lang['upload_exercise_files2'] =
     'Du kan lave øvelsesfiler med det Windows-baserede PC-program PLOTLearner.
      Øvelsesfiler har filtypen ».3et«.';
 $lang['upload_exercise_files3_top'] =
-    'Her kan du uploade øvelsesfiler til top-folderen på dette websted.';
+    'Her kan du uploade øvelsesfiler til den øverste mappe på dette websted.';
 $lang['upload_exercise_files3_other'] =
-    'Her kan du uploade øvelsesfiler til folderen \'%s\' på dette websted.';
+    'Her kan du uploade øvelsesfiler til mappen \'%s\' på dette websted.';
 $lang['upload_exercise_files4'] =
     'Klik på knappen »upload filer« her til højre for at vælge de filer der skal uploades. (I nogle browsere kan man også trække filer over på knappen og slippe dem dér.)';
-$lang['cannot_change_visibility_top'] = 'Man kan ikke ændre synligheden af top-folderen';
+$lang['cannot_change_visibility_top'] = 'Man kan ikke ændre synligheden af den øverste mappe';
 $lang['control_visibility'] = 'Styring af synlighed af øvelser';
-$lang['visibility_folder'] = 'Synlighed af folderen \'%s\'';
-$lang['visibility_folder_desc'] = 'Her kan du vælge hvilke klasser der kan se øvelserne i folderen \'%s\'.';
+$lang['visibility_folder'] = 'Synlighed af mappen \'%s\'';
+$lang['visibility_folder_desc'] = 'Her kan du vælge hvilke klasser der kan se øvelserne i mappen \'%s\'.';
 $lang['visible_to_class'] = 'Synlig for denne klasse?';
 $lang['class'] = 'Klasse';
 $lang['everybody'] = 'Alle';
 $lang['missing_old_filename'] = 'Gammelt filnavn mangler';
 $lang['missing_new_filename'] = 'Nyt filnavn mangler';
 $lang['illegal_char_filename'] = 'Ulovligt tegn i nyt filnavn';
-$lang['folders'] = 'Foldere';
-$lang['parent_folder'] = 'Øvre folder';
-$lang['delete_folder_button'] = 'Slet folder'; // Button text
-$lang['delete_folder_confirm'] = 'Slet folder'; // Title in confirmation dialog
-$lang['delete_folder_question'] = 'Ønsker du at slette folderen %s?';
-$lang['folder_not_empty'] = 'Folderen er ikke tom';
-$lang['create_folder_button'] = 'Opret folder'; // Button text
+$lang['folders'] = 'Mapper';
+$lang['parent_folder'] = 'Øvre mappe';
+$lang['delete_folder_button'] = 'Slet mappe'; // Button text
+$lang['delete_folder_confirm'] = 'Slet mappe'; // Title in confirmation dialog
+$lang['delete_folder_question'] = 'Ønsker du at slette mappen %s?';
+$lang['folder_not_empty'] = 'Mappen er ikke tom';
+$lang['create_folder_button'] = 'Opret mappe'; // Button text
 $lang['exercises'] = 'Øvelser';
 $lang['mark'] = 'Markering';
 $lang['name'] = 'Navn';
@@ -69,13 +69,13 @@ $lang['cancel_move'] = 'Afbryd flytning';
 $lang['cancel_copy'] = 'Afbryd kopiering';
 $lang['upload_exercises_button'] = 'Upload øvelser';
 $lang['create_exercise_button'] = 'Opret øvelse';
-$lang['create_folder_heading'] = 'Opret folder';
-$lang['folder_name_prompt'] = 'Foldernavn:';
+$lang['create_folder_heading'] = 'Opret mappe';
+$lang['folder_name_prompt'] = 'Mappenavn:';
 $lang['rename_file_heading'] = 'Omdøb fil';
 $lang['new_filename_prompt'] = 'Nyt filnavn:';
 $lang['create_exercise_heading'] = 'Opret øvelse';
 $lang['select_database_prompt'] = 'Vælg database:';
-$lang['click_and_go_to'] = 'Klik »OK« her og gå så til destinationsfolderen og klik på »%s«.';
+$lang['click_and_go_to'] = 'Klik »OK« her og gå så til destinationsmappen og klik på »%s«.';
 $lang['copy_files'] = 'Kopiér filer'; // Title in dialog heading
 $lang['move_files'] = 'Flyt filer'; // Title in dialog heading
 $lang['delete_file_confirm'] = 'Vil du slette de angivne filer?';
@@ -85,15 +85,15 @@ $lang['no_files_selected'] = 'Ingen filer valgt';
 $lang['confirm_deletion'] = 'Bekræft sletning';
 $lang['drop_files_here'] = 'Træk filer der skal oploades, hertil';
 $lang['upload_files_button'] = 'Upload filer';
-$lang['view_folder'] = 'Vis folder';
-$lang['cannot_open_folder'] = 'Kan ikke åbne folderen \'%s\'';
-$lang['cannot_create_folder'] = 'Kan ikke skabe folderen \'%s\'';
-$lang['illegal_folder'] = 'Ugyldig folder: %s';
-$lang['not_a_folder'] = 'Ikke en folder: %s';
+$lang['view_folder'] = 'Vis mappe';
+$lang['cannot_open_folder'] = 'Kan ikke åbne mappen \'%s\'';
+$lang['cannot_create_folder'] = 'Kan ikke skabe mappen \'%s\'';
+$lang['illegal_folder'] = 'Ugyldig mappe: %s';
+$lang['not_a_folder'] = 'Ikke en mappe: %s';
 $lang['access_denied_to'] = 'Der er ikke adgang til \'%s\'';
 $lang['already_exists'] = '\'%s\' findes allerede';
 $lang['cannot_rename'] = 'Kan ikke omdøbe \'%s\' til \'%s\'';
-$lang['cannot_delete_folder'] = 'Kan ikke slette folderen \'%s\'';
+$lang['cannot_delete_folder'] = 'Kan ikke slette mappen \'%s\'';
 $lang['cannot_delete_file'] = 'Kan ikke slette filen \'%s\'';
 $lang['ownership_updated'] = 'Ejerskab af øvelser opdateret';
 $lang['added_ownership'] = 'Ejerskab tilføjet for disse øvelser:';
