3c01a7262bc6c606013fdffa3fbc5fe497c9af03
Claus Tøndering <claus@ezer.dk>
2016-10-14 09:51:00 +0200

    -

--- myapp/core/MY_Lang.php
+++ myapp/core/MY_Lang.php
@@ -8,6 +8,101 @@ class MY_Lang extends CI_Lang {
         parent::__construct();
 	}
 
+    // This is a copy of the load function from CI_Lang. It is included here unchanged except the
+    // that it does not print an error if the language file is not found.
+	private function parent_load($langfile, $idiom = '', $return = FALSE, $add_suffix = TRUE, $alt_path = '')
+	{
+		if (is_array($langfile))
+		{
+			foreach ($langfile as $value)
+			{
+				$this->load($value, $idiom, $return, $add_suffix, $alt_path);
+			}
+
+			return;
+		}
+
+		$langfile = str_replace('.php', '', $langfile);
+
+		if ($add_suffix === TRUE)
+		{
+			$langfile = preg_replace('/_lang$/', '', $langfile).'_lang';
+		}
+
+		$langfile .= '.php';
+
+		if (empty($idiom) OR ! preg_match('/^[a-z_-]+$/i', $idiom))
+		{
+			$config =& get_config();
+			$idiom = empty($config['language']) ? 'english' : $config['language'];
+		}
+
+		if ($return === FALSE && isset($this->is_loaded[$langfile]) && $this->is_loaded[$langfile] === $idiom)
+		{
+			return;
+		}
+
+		// Load the base file, so any others found can override it
+		$basepath = BASEPATH.'language/'.$idiom.'/'.$langfile;
+		if (($found = file_exists($basepath)) === TRUE)
+		{
+			include($basepath);
+		}
+
+		// Do we have an alternative path to look in?
+		if ($alt_path !== '')
+		{
+			$alt_path .= 'language/'.$idiom.'/'.$langfile;
+			if (file_exists($alt_path))
+			{
+				include($alt_path);
+				$found = TRUE;
+			}
+		}
+		else
+		{
+			foreach (get_instance()->load->get_package_paths(TRUE) as $package_path)
+			{
+				$package_path .= 'language/'.$idiom.'/'.$langfile;
+				if ($basepath !== $package_path && file_exists($package_path))
+				{
+					include($package_path);
+					$found = TRUE;
+					break;
+				}
+			}
+		}
+
+        // The following lines have been removed with respect to the original
+		// if ($found !== TRUE)
+		// {
+		//     show_error('Unable to load the requested language file: language/'.$idiom.'/'.$langfile);
+		// }
+
+		if ( ! isset($lang) OR ! is_array($lang))
+		{
+			log_message('error', 'Language file contains no data: language/'.$idiom.'/'.$langfile);
+
+			if ($return === TRUE)
+			{
+				return array();
+			}
+			return;
+		}
+
+		if ($return === TRUE)
+		{
+			return $lang;
+		}
+
+		$this->is_loaded[$langfile] = $idiom;
+		$this->language = array_merge($this->language, $lang);
+
+		log_message('info', 'Language file loaded: language/'.$idiom.'/'.$langfile);
+		return TRUE;
+	}
+
+    
     private function load_from_db(array__OR__string $langfile, string $idiom, boolean $return, boolean $add_suffix /*ignored*/, string $alt_path /*ignored*/)
     {
         if ($idiom==='english')
@@ -29,7 +124,11 @@ class MY_Lang extends CI_Lang {
         $lang = array();
         foreach ($strings as $s)
             $lang[$s->key] = $s->text;
-            
+
+        if (empty($lang))
+            // Load from file instead of database
+            $lang = $this->parent_load($langfile, $idiom=='en' ? 'english' : $idiom, true, $add_suffix, $alt_path);
+
 		if ($return)
 			return $lang;
 
