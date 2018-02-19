fa9586abb6aab47c683d794d9dd08ced3068d5b2
Claus Tøndering <claus@ezer.dk>
2018-02-19 11:29:05 +0100

    Some German localization (retrieved from bibleole.3bmoodle.k on 2018-02-19).

---
+++ myapp/language/langsrc/de/class_lang.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['add_class'] = "Kurs hinzufügen";
---
+++ myapp/language/langsrc/de/menu_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['administration'] = "Administration";
+$lang['classes'] = "Kurse";
+$lang['display_text'] = "Text anzeigen";
+$lang['download_lexicon'] = "Wörterbuch downloaden";
+$lang['enroll_in_class'] = "Für Kurs anmelden/abmelden ";
+$lang['exercises'] = "Übungen";
+$lang['font_preferences'] = "Schriftart einstellen";
+$lang['home'] = "Startseite";
+$lang['login'] = "Anmelden";
+$lang['logout'] = "Abmelden";
+$lang['manage_exercises'] = "Übungen verwalten";
+$lang['my_data'] = "Meine Daten";
+$lang['my_progress'] = "Mein Leistungsstand";
+$lang['privacy_policy'] = "Datenschutzrichtlinie";
+$lang['profile'] = "Profil";
+$lang['statistics'] = "Statistiken";
+$lang['students_progress'] = "Leistungsstand der Studenten";
+$lang['text_and_exercises'] = "Text und Übungen";
+$lang['translate_grammar'] = "Übersetzen grammatischer Terminologien";
+$lang['translate_interface'] = "Übersetze Oberfläche";
+$lang['translate_lexicon'] = "Übersetze Wörterbuch";
+$lang['users'] = "Nutzer";
+$lang['user_access'] = "Nutzerzugang";
