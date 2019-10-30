bd9abd1ed06744f69f4d7a8fe3b242317d3fe736
Claus Tøndering <claus@ezer.dk>
2019-10-30 15:31:24 +0100

    Updated Dutch lexicons and German localization

--- myapp/language/langsrc/de/class_lang.php
+++ myapp/language/langsrc/de/class_lang.php
@@ -1,11 +1,15 @@
 <?php
 
 $lang['add_class'] = "Kurs hinzufügen";
+$lang['assign_users'] = "Nutzer zuordnen";
 $lang['change_owner_class'] = "Eigentümer wechseln";
-$lang['classes'] = "Klassen";
+$lang['classes'] = "Kurse";
 $lang['class_delete'] = "Löschen";
-$lang['class_list'] = "Klassenliste";
-$lang['class_name'] = "Klassenname";
-$lang['class_pw'] = "Klassenpasswort";
-$lang['delete_class'] = "Klasse löschen";
+$lang['class_edit'] = "Bearbeiten";
+$lang['class_list'] = "Kursliste";
+$lang['class_name'] = "Kursname";
+$lang['class_pw'] = "Kurspasswort";
+$lang['configure_your_classes'] = "deine Kurse bearbeiten";
+$lang['delete_class'] = "Kurs löschen";
+$lang['edit_class'] = "Kurs bearbeiten";
 $lang['enroll_before'] = "Registriere bevor";
