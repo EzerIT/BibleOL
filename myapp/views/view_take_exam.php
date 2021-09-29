e806f788a6700289beaa23ca9de712d21aee8b30
Adrian Negrea <mradriannegrea@gmail.com>
2021-09-29 09:06:16 -0400

    Hide timer when exam is done.

--- myapp/views/view_take_exam.php
+++ myapp/views/view_take_exam.php
@@ -4,8 +4,10 @@
   Time Left: <p id="timeLeft"></p>
 </div>
 
+<p id="test"></p>
+
 <div>
-<object id="quiz_frame" data="/exams/show_quiz?quiz=<?= str_replace("/", "%2F", array_shift($exercises)) ?>&examid=<?= $exam_id ?>&exercise_lst=<?= str_replace("/", "%2F", implode("~",$exercises)) ?>" width="100%" height="auto" onLoad="loaded()">
+<object id="quiz_frame" data="/exams/show_quiz?quiz=<?= str_replace("/", "%2F", array_shift($exercises)) ?>&examid=<?= $exam_id ?>&exercise_lst=<?= str_replace("/", "%2F", implode("~",$exercises)) ?>" width="100%" height="auto" onLoad="loaded()" onclick="frameClick()">
 </object>
 </div>
 
@@ -13,6 +15,10 @@
 <script>
 var deadline = <?= $deadline ?>;
 setInterval(function(){
+    var frame = document.getElementById('quiz_frame');
+    if (frame.contentWindow.document.getElementById('done') !== null) {
+      document.getElementById('timer').style.display = 'none';
+    }
     timeLeft= deadline - new Date().getTime() / 1000;
     if (timeLeft < 0) {
       document.getElementById('quiz_frame').contentWindow.document.getElementById('finish').click();
@@ -28,14 +34,7 @@ setInterval(function(){
 
 function loaded(){
   var frame = document.getElementById("quiz_frame");
-  if (frame.data == "exams/exam_done") {
-    document.getElementById('timer').style.display = 'none';
-  }
   frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
-  var quizid = document.getElementById("quiz_frame").contentWindow.document.getElementById("quiz_id").innerHTML;
-  document.getElementById("quiz_frame").contentWindow.document.getElementById("finish").addEventListener("click", function() {
-    document.getElementById("quizid").value = document.getElementById("quizid").value + ',' + quizid;
-  })
 }
 
 </script>
