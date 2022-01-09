00023b806d8b8e797efe13e576d28b943fc96981
Oliver Glanz <o.glanz@gmail.com>
2022-01-09 17:22:22 -0500

    Fixed language files to display correctly finish section in exams

--- myapp/views/view_text_display.php
+++ myapp/views/view_text_display.php
@@ -63,7 +63,7 @@
       <div id="quizcontainer"></div>
       <div id="buttonlist2">
           <button id="next_question" class="btn btn-quiz" type="button"><?= $this->lang->line('next') ?></button>
-          <button id="finish" class="btn btn-quiz" type="button"><?= $this->lang->line('finish') ?></button>
+          <button id="finish" class="btn btn-quiz" type="button"><?= isset($is_exam) && $is_exam ? $this->lang->line('finish_section') : $this->lang->line('finish') ?></button>
           <?php if (!(isset($is_exam) && $is_exam)): ?>
             <button id="finishNoStats" class="btn btn-quiz" type="button"><?= $this->lang->line('finish_no_grading') ?></button>
           <?php endif; ?>
