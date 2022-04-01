e7787efff7c8dfe1ba55eb82595352c594718e36
Mabio Coelho <mabiorc@gmail.com>
2022-04-01 12:30:33 -0400

    Detail of quizzes

--- myapp/views/view_grades_teacher_exams.php
+++ myapp/views/view_grades_teacher_exams.php
@@ -263,7 +263,7 @@
           <td><?= $st ?></td>
           <td class="text-center"><?= Statistics_timeperiod::format_time($startTime) ?></td>
           <td class="text-center"><?= round($tot_percWeighted/$tot_weight) . "% (" .  round($tot_percent/$ncounter)  ?>%)</td>
-          <td class="text-center"><?= calculateGrade($grade_system, ($tot_percWeighted/$tot_weight)) ?></td>
+          <td class="text-center"><?php echo anchor(build_get('grades/teacher_quizz_detail/classid/' . $classid . '/quizzid/'.$result["quizzid"] . '/userid/'.$result["userid"], array() ), calculateGrade($grade_system, ($tot_percWeighted/$tot_weight)));?></td>
           <td class="text-center"><?= $result["duration"] ?></td>
           <td class="text-center"><?= sprintf("%.1f",round(60/($tot_featpMin/$ncounter))) ?></td>
           <td class="text-center">
