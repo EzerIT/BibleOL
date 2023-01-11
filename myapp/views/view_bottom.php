87de94ae7bdc0b045b309ae8f73e346bd622ce45
Claus Tøndering <claus@tondering.dk>
2023-01-11 09:36:18 +0100

    Changed footer text

--- myapp/views/view_bottom.php
+++ myapp/views/view_bottom.php
@@ -11,7 +11,7 @@
         <div class="row">
           <div class="col-md-4 footer footer-column">
             <h2>About BibleOL</h2>
-            <p>BibleOL is an online learning platform for training the Biblical languages Hebrew and Greek and also Latin. It utilizes top-notch language databases to provide students the best learning experience.</p>
+            <p>BibleOL is an online learning platform that assists students learning biblical Hebrew and Greek, as well as Latin. It uses advanced language databases to deliver an effective learning experience to students.</p>
             <p>Enjoy!</p>
             <a href="/privacy">| Privacy Policy |</a>
           </div>
