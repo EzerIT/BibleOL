d6ac084444dcb2682037a5c4ecdf51756dd0d9e5
tmccormack165 <tmccormack16@gmail.com>
2023-12-01 12:47:28 -0500

    Docker Project for local development

---
+++ bibleol-docker/config.sh
@@ -0,0 +1,27 @@
+# Start Apache and MySQL
+sudo systemctl is-enabled mysql.service || systemctl enable mysql.service
+sudo systemctl status mysql.service || sudo systemctl start mysql.service
+sudo service apache2 start
+
+# Apache Configuration
+sudo a2enmod ssl rewrite
+sudo a2dissite 000-default 
+sudo a2ensite bibleol 
+sudo service apache2 reload
+
+# MySQL Configuration
+echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql 
+echo "CREATE DATABASE ${MYSQL_DATABASE}" | mysql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@localhost;" | mysql
+cd /var/www/html/BibleOL/myapp/config
+sudo cp database.php-dist database.php
+sudo sed -i -e "s/USERNAME/${MYSQL_USER}/g" database.php 
+sudo sed -i -e "s/PASSWORD/${MYSQL_PASSWORD}/g" database.php 
+sudo sed -i -e "s/DATABASE/${MYSQL_DATABASE}/g" database.php 
+sudo sed -i -e "s/localhost/localhost:3306/g" database.php
+sudo cp ol.php-dist ol.php
+sudo cp config.php-dist config.php
+sudo sed -i -e "s@https://example.com@${BASE_URL}@g" config.php
+cd /var/www/html/BibleOL
+sudo mysql ${MYSQL_DATABASE} < bolsetup.sql
+sudo ./setup_lang.sh
