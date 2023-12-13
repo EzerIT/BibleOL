536fe98882080e49d60d70aa1f5db4e4b63c54da
Timothy McCormack <timothy.mccormack@panartis.com>
2023-12-13 14:12:18 -0500

    finished AMD container

--- Dockerfiles/bibleol-amd-buildtime/start.sh
+++ Dockerfiles/bibleol-amd-buildtime/start.sh
@@ -3,25 +3,6 @@ sudo systemctl is-enabled mysql.service || systemctl enable mysql.service
 sudo systemctl status mysql.service || sudo systemctl start mysql.service
 sudo service apache2 start
 
-# Apache Configuration
-sudo a2enmod ssl rewrite
-sudo a2dissite 000-default 
-
-cd /var/www/html && sudo git clone --recursive https://github.com/EzerIT/BibleOL && \
-    cd BibleOL && \
-    bash git-hooks/setup.sh && \
-    sudo cp .htaccess-dist .htaccess && \
-    sudo apt install -y npm && \
-    sudo npm install -g -y less && \
-    sudo npm install -g -y typescript && \
-    sudo npm install -y @types/bootstrap@4.5.0 @types/jquery @types/jqueryui
-
-sudo chown www-data:www-data /var/lib/php/sessions
-sudo mv /bibleol.conf /etc/apache2/sites-available/
-
-sudo a2ensite bibleol 
-sudo service apache2 reload
-
 # MySQL Configuration
 echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql 
 echo "CREATE DATABASE ${MYSQL_DATABASE}" | mysql
@@ -38,3 +19,5 @@ sudo sed -i -e "s@https://example.com@${BASE_URL}@g" config.php
 cd /var/www/html/BibleOL
 sudo mysql ${MYSQL_DATABASE} < bolsetup.sql
 sudo ./setup_lang.sh
+
+sudo service apache2 start
---
+++ Dockerfiles/bibleol-amd-multistage/clean_windows.sh
@@ -0,0 +1,2 @@
+docker container rm -f bibleol-amd-container
+docker image rm -f tmccormack14/bibleol-amd-img:2023_12_08
