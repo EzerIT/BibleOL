44ab95018eb9e5976613f3b88e2a9baee7713591
tmccormack165 <tmccormack16@gmail.com>
2024-03-11 17:37:35 +0700

    update to user guide

---
+++ usersguide/en/AU_exam_greek.php
@@ -0,0 +1,2 @@
+<p>Work in progress</p>
+
---
+++ usersguide/en/exam_introduction.php
@@ -0,0 +1,9 @@
+<h1>Introduction to Exam</h1>
+
+<p>BibleOL offers the options for creating and taking exam. While each instructor can create exams for students, institutions can use the exam feature to build exams that can be used as Qualifiers for incoming students or as standardized exams that all instructors use in their institutional courses.</p>
+
+<ul>
+	<li>If you are a student at Andrews University go to the &ldquo;Andrews University Final Hebrew Exam&rdquo; if you are taking the course OTST552.</li>
+	<li>If you are a student at Andrews University go to the &ldquo;Andrews University Final Greek Exam&rdquo; if you are taking the course NTST552.</li>
+	<li>If you are an exisiting or prospect student at Andrews University go to &ldquo;Andrews University Hebrew Placement Exam&rdquo; if you signed up for the Hebrew Placement Exam.</li>
+</ul>
