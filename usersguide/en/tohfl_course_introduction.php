e12e72c620e5a2470c0988a1ecd7dac2337b49b6
tmccormack165 <tmccormack16@gmail.com>
2024-05-15 12:55:33 +0900

    updated userguide

---
+++ usersguide/en/tohfl_course_introduction.php
@@ -0,0 +1,37 @@
+<h2>Becoming a Student of the Hebrew TOHFL course</h2>
+
+<h5>1. Creating a User Account</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/a96RG5gw-tA" width="560"></iframe></p>
+
+<p>&nbsp;</p>
+
+<h5>2. Changing your User Profile: Make sure you choose the profile &ldquo;AndrewsUniversity&rdquo;</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/UYE69Sg-W8M" width="560"></iframe></p>
+
+<p>&nbsp;</p>
+
+<h5>3. Enrolling in a Class</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/e-8zfi4U5uc" width="560"></iframe></p>
+
+<p>=&gt; Enroll into the class <em>OTST551-Heb-I_2024-02(summer)_glanz</em>:</p>
+
+<p>&nbsp;</p>
+
+<h5>4. How to take an exercise</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/qG3kExRAWqk" width="560"></iframe></p>
+
+<p>&nbsp;</p>
+
+<h5>5. Looking up your exercise scores/grades</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/taeG9A7atsg" width="560"></iframe></p>
+
+<p>&nbsp;</p>
+
+<h5>6. How to take an exam</h5>
+
+<p><iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/LLcumsc4LkA?si=u8G_TnnxrF-v8gco" width="560"></iframe></p>
