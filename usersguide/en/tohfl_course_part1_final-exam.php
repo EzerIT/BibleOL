abfdb9d2829643def57c5cabb4259233aa5c4fc0
tmccormack165 <tmccormack16@gmail.com>
2024-05-17 17:11:22 +0900

    update user guide

---
+++ usersguide/en/tohfl_course_part1_final-exam.php
@@ -0,0 +1,93 @@
+<h1>TOHFL-I Final Exam</h1>
+
+<p>The TOHFL-I Final Exam consists of the following eight elements:</p>
+
+<ul>
+	<li><strong>Vocabulary</strong>: Students will be asked 20 words. Students need to provide glosses they have learned as part of their vocab training. Students must use the TOHFL-I vocab booklet to make sure their learned glosses match the ones in the BibleOL.</li>
+	<li><strong>Morphology Prefixing</strong>: Students must choose the right vowel+consonant combinations for prefixing prepositions and conjunctions to words. 10 words need to be prefixed correctly.</li>
+	<li><strong>Morphology Nouns</strong>: Students must analyze 10 nominal forms (covering all states, number, gender).</li>
+	<li><strong>Morphology Suffixation</strong>: Students must analyze 5 suffixes attached to nominal forms (person, number, gender).</li>
+	<li><strong>Verbal Morphology</strong>: Students must answer 16 verbal forms (covering all tenses, all stems, all verbal classes, suffixed and non-suffixed forms).</li>
+	<li><strong>Subphrasre Analysis</strong>: Students must analyze 10 subphrase relations (is the word relation part of a construct relation, attributive-adjective relation, a demonstrative relation, or something else).</li>
+	<li><strong>Phrase analysis</strong>: Students must translate 10 clauses by identifying the phrase function of the different phrases in simple sentences. BibleOL will provide the meaning of unknown words if they do appear.</li>
+	<li><strong>Clause relation analysis</strong>: Student must translate ~6 sentences by identifying clause relations (independent vs. dependent clause [relative clause, object clause)</li>
+</ul>
+
+<p>Students can exercise with the final exam materials and get a realistic sense of the final exam before the actual exam day. If a student&rsquo;s performance is on a top level when doing the exercises, the student will know that she or he will perform well in the final new. No surprises should be expected. This is because the data pool for the exam-prep exercises are identical to the actual exam.</p>
+
+<p>The table below provides access to each ot the eight parts of the exam. Each exam part is <em><strong>hyperlinked</strong></em> so that students can get started with exam preparations.</p>
+
+<p>&nbsp;</p>
+
+<table align="left" border="1" cellpadding="1" cellspacing="1" style="width:1800px">
+	<tbody>
+		<tr>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_01_SDATS_Vocabulary.3et&amp;count=20" target="_blank">vocabulary</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_24_SDATS_morphology_prefixing_WawHaBeKeLeMin_multiplechoice.3et&amp;count=10" target="_blank">morphology: prefixing</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_31_SDATS_morphology_nominal-forms.3et&amp;count=10" target="_blank">morphology: nouns</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_32_SDATS_morphology_suffixation_nouns-prepositions.3et&amp;count=5" target="_blank">morphology: suffixation</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_41_SDATS_morphology_verbal-forms.3et&amp;count=16" target="_blank">morphology: verbs</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_50_SDATS_syntax_subphrase-analysis.3et&amp;count=10" target="_blank">syntax: subphrase relations</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_51_SDATS_syntax_phrase-analysis.3et&amp;count=10" target="_blank">syntax: phrase functions</a></strong></td>
+			<td><strong><a href="https://learner.bible/text/show_quiz?quiz=Andrews%20University%2F0_Hebrew-Qualifier%2FHebrew-01_52_SDATS_syntax_clause-connection-analysis.3et&amp;count=10" target="_blank">syntax:clause relations</a></strong></td>
+		</tr>
+		<tr>
+        <td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_01_vocab.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+            
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_02_prefixing.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_03_n-morphology.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_04_s-morphology.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_05_v-morphology.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_06_subphrase.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+            
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_07_phrase.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+			<td style="vertical-align:top; width:225px;">
+			<figure class="easyimage easyimage-full"><?= $hdir->img("AU_hebrew-exam_otst551_08_clause.png") ?>
+			<figcaption></figcaption>
+			</figure>
+			</td>
+
+		</tr>
+	</tbody>
+</table>
+
+<p>&nbsp;</p>
+
+<h2>&nbsp;</h2>
+
+<h2>&nbsp;</h2>
+
+<h2>&nbsp;</h2>
