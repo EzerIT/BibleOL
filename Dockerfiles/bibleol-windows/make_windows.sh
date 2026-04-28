f5ac5a30546cf108ae2e219d99aa718a4e8d8e40
Mabio Coelho <mabiorc@gmail.com>
2026-04-27 20:53:44 -0400

    iUpdate make build 2 to fix a erro if not on home

--- Dockerfiles/bibleol-windows/make_windows.sh
+++ Dockerfiles/bibleol-windows/make_windows.sh
@@ -4,3 +4,6 @@ container_name="bibleol-windows-container"
 docker image build -t $image_name .
 docker container run -dt -p 8000:80 --env-file environment.txt --name $container_name $image_name
 docker container exec $container_name bash start.sh
+
+# If you want to run with volume mapping (equivalent to make build2):
+# powershell -Command "$p = Join-Path (Get-Location) '../../' | Resolve-Path; docker container run -dt -p 8000:80 --env-file environment.txt --volume \"$($p.Path):/var/www/html/BibleOL\" --name $container_name $image_name"
